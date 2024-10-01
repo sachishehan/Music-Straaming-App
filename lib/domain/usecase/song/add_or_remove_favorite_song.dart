ef4802ec0999dfc4b3c3fc4ddc1a50b52969aef2
sachishehan <shehansachintha0@gmail.com>
2024-10-02 00:48:11 +0530

    options

--- lib/domain/usecase/song/add_or_remove_favorite_song.dart
+++ lib/domain/usecase/song/add_or_remove_favorite_song.dart
@@ -1,8 +1,8 @@
 import 'package:dartz/dartz.dart';
-import 'package:spotify/core/usecase/usecase.dart';
+import 'package:sportify/core/usecase/usecase.dart';
+import 'package:sportify/data/repository/song/song.dart';
 
 import '../../../service_locator.dart';
-import '../../repository/song/song.dart';
 
 class AddOrRemoveFavoriteSongUseCase implements UseCase<Either,String> {
   @override
