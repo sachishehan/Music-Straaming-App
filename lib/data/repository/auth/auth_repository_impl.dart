68ebfc75988f01d8623480e3eb91665abe335449
sachishehan <shehansachintha0@gmail.com>
2024-10-05 10:40:34 +0530

    repo

---
+++ lib/data/repository/auth/auth_repository_impl.dart
@@ -0,0 +1,28 @@
+import 'package:dartz/dartz.dart';
+import 'package:sportify/data/models/auth/create_user_req.dart';
+import 'package:sportify/data/models/auth/signin_user_req.dart';
+import 'package:sportify/data/sources/auth/auth_firebase_service.dart';
+import 'package:sportify/domain/repository/auth/auth.dart';
+
+
+import '../../../service_locator.dart';
+
+class AuthRepositoryImpl extends AuthRepository {
+
+  
+  @override
+  Future<Either> signin(SigninUserReq signinUserReq) async {
+     return await sl<AuthFirebaseService>().signin(signinUserReq);
+  }
+
+  @override
+  Future<Either> signup(CreateUserReq createUserReq) async {
+    return await sl<AuthFirebaseService>().signup(createUserReq);
+  }
+  
+  @override
+  Future<Either> getUser() async {
+    return await sl<AuthFirebaseService>().getUser();
+  }
+  
+}
