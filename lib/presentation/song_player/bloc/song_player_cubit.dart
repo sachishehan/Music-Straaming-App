72ac28667f7c963c63ec66990fc5cb5c44381b80
sachishehan <shehansachintha0@gmail.com>
2024-10-11 10:53:39 +0530

    song player

--- lib/presentation/song_player/bloc/song_player_cubit.dart
+++ lib/presentation/song_player/bloc/song_player_cubit.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:just_audio/just_audio.dart';
-import 'package:spotify/presentation/song_player/bloc/song_player_state.dart';
+import 'package:sportify/presentation/song_player/bloc/song_player_state.dart';
 
 class SongPlayerCubit extends Cubit<SongPlayerState> {
 
